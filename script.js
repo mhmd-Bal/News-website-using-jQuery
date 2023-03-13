aea094870e199d209c0699064aa28b39ed024e55
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-13 14:41:17 +0200

    Added untested Jquery to add the news

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$.get("GetAllNews.php", PrintAllNews(data){
-  
+$.getJSON("GetAllNews.js", function(result){
+
+  let news_block = $("<div></div>");
+  $(".Wrapper").append(news_block);
+
+  $.each(result, function(i, field){
+    news_block.append(field + "</br>");
+  });
 });
